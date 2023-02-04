b98f3e1866252b89adac6905fdf95af8235585ef
Andrzej Nogala <nogala.andrzej@gmail.com>
2023-02-04 10:19:17 +0100

    added table with statistics players

--- js/main.js
+++ js/main.js
@@ -86,7 +86,6 @@ $('.enemy').on('click', function(e) {
             battlefield.store(bandit.getRandomObject());
             message = 'Dodano Bandytę do listy';
             break;
-            BattlefieldStorage
         case 'scout-bandit':
             const scoutBandit = new ScoutBanditGenerator(app);
             battlefield.store(scoutBandit.getRandomObject());
@@ -278,4 +277,117 @@ $(document.body).on('click', '.show-statisticks', function(e){
 
 $('.click-to-clear-value').on('click', function(e) {
     $(e.target).val('');
+});
+
+$('#showPlayersStatistics').on('click', function(e) {
+    const players = app.getPlayersName();
+
+    let html = '';
+    let head = '';
+    let body = '';
+    let body_2 = '';
+    let playerObj = {};
+    let generator = {};
+    let strength = '<th class="tg-0pky">str</th>';
+    let dexterity = '<th class="tg-0pky">dex</th>';
+    let condition = '<th class="tg-0pky">con</th>';
+    let intelligence = '<th class="tg-0pky">int</th>';
+    let wisdom = '<th class="tg-0pky">wis</th>';
+    let charisma = '<th class="tg-0pky">chr</th>';
+
+    let pp = '<th class="tg-0pky">pp</th>';
+    let pi = '<th class="tg-0pky">pi</th>';
+    let passiveIntuition = null;
+
+    players.forEach(player => {
+        if ('kreatura' === player) {
+            generator = new KreaturaGenerator(app);
+            passiveIntuition = 18;
+        } else if ('omalen' === player) {
+            generator = new OmalenGenerator(app);
+            passiveIntuition = 13;
+        } else if ('hum' === player) {
+            generator = new HumGenerator(app);
+            passiveIntuition = 9;
+        } else if ('alsariph' === player) {
+            generator = new AlsariphGenerator(app);
+            passiveIntuition = 11;
+        }
+        playerObj = generator.getRandomObject();
+
+        head += `<th class="tg-0pky">${playerObj.name}</th>`;
+
+        strength += `<th class="tg-0pky">${playerObj.statistics.strength}</th>`;
+        dexterity += `<th class="tg-0pky">${playerObj.statistics.dexterity}</th>`;
+        condition += `<th class="tg-0pky">${playerObj.statistics.condition}</th>`;
+        intelligence += `<th class="tg-0pky">${playerObj.statistics.intelligence}</th>`;
+        wisdom += `<th class="tg-0pky">${playerObj.statistics.wisdom}</th>`;
+        charisma += `<th class="tg-0pky">${playerObj.statistics.charisma}</th>`;
+
+        pp += `<th class="tg-0pky">${playerObj.statistics.pp}</th>`;
+        pi += `<th class="tg-0pky">${passiveIntuition}</th>`;
+    });
+
+
+    body += `
+    <tr>
+        ${strength}
+    </tr>
+    <tr>
+        ${dexterity}
+    </tr>
+    <tr>
+        ${condition}
+    </tr>
+    <tr>
+        ${intelligence}
+    </tr>
+    <tr>
+        ${wisdom}
+    </tr>
+    <tr>
+        ${charisma}
+    </tr>`;
+
+    body_2 += `
+    <tr>
+        ${pp}
+    </tr>
+    <tr>
+        ${pi}
+    </tr>`;
+
+    html += `
+    <div class="row">
+    <div class="col-12">
+        <table class="tg">
+            <thead>
+                <tr>
+                    <th class="tg-0pky">---</th>
+                    ${head}
+                </tr>
+            </thead>
+            <tbody>
+                ${body}
+            </tbody>
+        </table>
+    </div>
+    </div>
+    <div class="row">
+    <div class="col-12">
+        <table class="tg">
+            <thead>
+                <tr>
+                    <th class="tg-0pky">---</th>
+                    ${head}
+                </tr>
+            </thead>
+            <tbody>
+                ${body_2}
+            </tbody>
+        </table>
+    </div>
+    </div>
+    `;
+    app.renderCache(html);
 });
